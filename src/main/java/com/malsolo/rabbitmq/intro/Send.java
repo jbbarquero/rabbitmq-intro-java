dfcc05e4902c6e7ccef4c99e86583d7acba2c2ad
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-07-28 01:17:51 +0200

    rabbitmq-intro-java: 1. Hello World! Just simply configure loggers

--- src/main/java/com/malsolo/rabbitmq/intro/Send.java
+++ src/main/java/com/malsolo/rabbitmq/intro/Send.java
@@ -3,14 +3,19 @@ package com.malsolo.rabbitmq.intro;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class Send {
+    static Logger logger = LoggerFactory.getLogger(Send.class);
+
     private final static String QUEUE_NAME = "hello_from_java";
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        logger.info("Java sender begins");
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
@@ -22,5 +27,6 @@ public class Send {
 
         channel.close();
         connection.close();
+        logger.warn("Java sender ends");
     }
 }
