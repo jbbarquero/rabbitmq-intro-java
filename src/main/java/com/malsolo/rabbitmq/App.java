d85632221f8fb9f944898071b9bd09b71f6e136f
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-07-28 00:45:56 +0200

    rabbitmq-intro-java: mvn project updated to java 8

--- src/main/java/com/malsolo/rabbitmq/App.java
+++ src/main/java/com/malsolo/rabbitmq/App.java
@@ -1,13 +1,18 @@
 package com.malsolo.rabbitmq;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n*2)
+                .limit(2)
+                .forEach(System.out::println);
     }
 }
