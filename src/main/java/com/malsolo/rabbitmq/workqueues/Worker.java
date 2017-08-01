c0e473b6027a2207e4a73dcc39bf4610f540f2c4
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-08-02 00:20:23 +0200

    rabbitmq-intro-java: 2. Work queues. Fair dispatch (basicQos: prefetchCount)

--- src/main/java/com/malsolo/rabbitmq/workqueues/Worker.java
+++ src/main/java/com/malsolo/rabbitmq/workqueues/Worker.java
@@ -20,6 +20,9 @@ public class Worker {
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
+
         final Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
