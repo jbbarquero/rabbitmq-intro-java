e446b412e19c34d29460e9ee7451500cf3da1884
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-08-01 17:52:31 +0200

    rabbitmq-intro-java: 2. Work queues. Sendere (NewTask)

---
+++ src/main/java/com/malsolo/rabbitmq/Queues.java
@@ -0,0 +1,5 @@
+package com.malsolo.rabbitmq;
+
+public enum Queues {
+    hello_queue, work_queues
+}
---
+++ src/main/java/com/malsolo/rabbitmq/workqueues/NewTask.java
@@ -0,0 +1,41 @@
+package com.malsolo.rabbitmq.workqueues;
+
+import com.google.common.base.Joiner;
+import com.malsolo.rabbitmq.Queues;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class NewTask {
+
+    static Logger logger = LoggerFactory.getLogger(NewTask.class);
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        logger.info("Java work queues begins");
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(Queues.work_queues.name(), false, false, false, null);
+
+        String message = getMessage(args);
+
+        channel.basicPublish("", Queues.work_queues.name(), null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+    }
+
+    private static String getMessage(String[] strings) {
+        if (strings == null || strings.length < 1) {
+            return "Hello World!";
+        }
+        Joiner joiner = Joiner.on(" ").skipNulls();
+        return joiner.join(strings);
+    }
+}
